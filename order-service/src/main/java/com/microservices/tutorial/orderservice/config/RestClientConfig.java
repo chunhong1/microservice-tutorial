d84fa5447e0101b2a555f08bf682b686a9248c01
Yeap-Chun-Hong <chunhong2002@outlook.com>
2025-09-25 20:28:32 +0800

    Feat: Circuit Breaker with Resilience4J

--- order-service/src/main/java/com/microservices/tutorial/orderservice/config/RestClientConfig.java
+++ order-service/src/main/java/com/microservices/tutorial/orderservice/config/RestClientConfig.java
@@ -2,11 +2,18 @@ package com.microservices.tutorial.orderservice.config;
 
 import com.microservices.tutorial.orderservice.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.http.client.ClientHttpRequestFactorySettings;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig
@@ -24,5 +31,13 @@ public class RestClientConfig
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+
+    private ClientHttpRequestFactory getClientRequestFactory()
+    {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(3000); // 3 seconds
+        factory.setReadTimeout(3000);    // 3 seconds
+        return factory;
     }
 }
