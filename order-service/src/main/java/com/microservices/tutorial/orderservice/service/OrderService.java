fdf39458c569cd27fd67b9becb4096203afcada3
chunhong1 <chunhong2002@outlook.com>
2025-10-02 20:33:41 +0800

    Feat: Async Communication using Kafka with Schema Registry

--- order-service/src/main/java/com/microservices/tutorial/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/microservices/tutorial/orderservice/service/OrderService.java
@@ -2,12 +2,17 @@ package com.microservices.tutorial.orderservice.service;
 
 import com.microservices.tutorial.orderservice.client.InventoryClient;
 import com.microservices.tutorial.orderservice.dto.OrderRequest;
+import com.microservices.tutorial.orderservice.event.OrderPlacedEvent;
 import com.microservices.tutorial.orderservice.model.Order;
 import com.microservices.tutorial.orderservice.repository.OrderRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Service
@@ -15,8 +20,10 @@ import java.util.UUID;
 @Transactional
 public class OrderService
 {
+    private static final Logger log = LoggerFactory.getLogger(OrderService.class);
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest)
     {
@@ -26,10 +33,19 @@ public class OrderService
         {
             Order order = new Order();
             order.setOrderNumber(UUID.randomUUID().toString());
-            order.setPrice(orderRequest.price());
+            order.setPrice(orderRequest.price().multiply(BigDecimal.valueOf(orderRequest.quantity())));
             order.setSkuCode(orderRequest.skuCode());
             order.setQuantity(orderRequest.quantity());
             orderRepository.save(order);
+
+            // Send message to Kafka Topic
+            var orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(),
+                    orderRequest.userDetails().email(),
+                    orderRequest.userDetails().firstName(),
+                    orderRequest.userDetails().lastName());
+            log.info("Start- Sending OrderPlacedEvent {} to Kafka Topic", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End- Sending OrderPlacedEvent {} to Kafka Topic", orderPlacedEvent);
         }else
         {
             throw new RuntimeException("Product with SKU Code " + orderRequest.skuCode() + " is not in stock.");
